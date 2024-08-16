0fe23fa3df00241c81659a9f8a0cfee7f2d5ce93
theskid22 <shrey150436@gmail.com>
2024-08-15 20:57:07 -0400

    version v3.1.2 (config stuff)

---
+++ static/config.js
@@ -0,0 +1,9 @@
+const htmlConfig = {
+    gamesPath: "/g.html",
+    creditsPath: "/credits.html",
+    changeLogPath: "/ChangeLog.html",
+    homePagePath: "/index.html",
+    moviesPath: "/movies.html",
+    searchPath: "/p.html",
+    settingsPath: "/settings.html",
+}
