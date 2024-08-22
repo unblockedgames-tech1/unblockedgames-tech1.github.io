c610cf91ab0730d2a28f386c588a09fd9a6de8bb
theskid22 <shrey150436@gmail.com>
2024-08-22 13:40:19 -0400

    please dont crash again

--- index.js
+++ index.js
@@ -6,28 +6,24 @@ const app = express();
 const bare = createBareServer("/bare/");
 const server = http.createServer();
 const PORT = 8080;
-
-
-
-app.use(express.static("static"));
-app.use(express.static("img"));
-
-app.get('*', function(req, res) {
-	res.send('404: Not Found');
-});
-
-server.on("request", (req, res) => {
+function setupServer() {
+  app.use(express.static("static"))
+  app.use(express.static("img"))
+  app.get('*', function(req, res) {res.send("404: not found")})
+  server.on("request", (req, res) => {
     if (bare.shouldRoute(req)) {
-        bare.routeRequest(req, res);
+      bare.routeRequest(req, res)
     } else {
-        app(req, res);
+      app(req, res)
     }
-});
+  })
+  server.listen({port: PORT}, () => {})
+}
+try {
 
+setupServer()
 
-// Ik theres a way to not hardcode ports but Idgaf
-server.on("listening", () => {
-    console.log('Listening on 8080');
-});
-
-server.listen({ port: PORT }, () => {});
+}
+catch (e) {
+  setupServer()
+}
