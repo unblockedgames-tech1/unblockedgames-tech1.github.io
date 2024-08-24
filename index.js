7f260cb1447b3043e5b96589c0f7fbf21711aa62
theskid22 <shrey150436@gmail.com>
2024-08-24 19:45:07 -0400

    redid backend

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 import { createBareServer } from '@tomphttp/bare-server-node';
 import express from 'express';
 import http from 'node:http';
+import path from "node:path";
+
 
 const app = express();
 const bare = createBareServer("/bare/");
 const server = http.createServer();
+const __dirname = process.cwd();
 const PORT = 8080;
-function setupServer() {
-  app.use(express.static("static"))
-  app.use(express.static("img"))
-  app.get('*', function(req, res) {res.send("404: not found")})
-  server.on("request", (req, res) => {
-    if (bare.shouldRoute(req)) {
-      bare.routeRequest(req, res)
-    } else {
-      app(req, res)
-    }
-  })
-  server.listen({port: PORT}, () => {})
-}
-try {
 
-setupServer()
+app.use(express.static("img")) // IMGS GET PROIRITY BI
+app.get("/ga", function (req, res) {
+  res.sendFile(path.join(__dirname, "static/games.html"));
+})
+app.get("/ta", function (req, res) {
+  res.sendFile(path.join(__dirname, "static/proxy.html"));
+})
+app.get("/app", function (req, res) {
+  res.sendFile(path.join(__dirname, "static/apps.html"));
+})
+app.get("/credits", function (req, res) {
+  res.sendFile(path.join(__dirname, "static/credits.html"));
+})
+app.get("/settings", function (req, res) {
+  res.sendFile(path.join(__dirname, "static/settings.html"));
+})
+app.use(express.static(path.join(__dirname, "static")));
 
-}
-catch (e) {
-  setupServer()
-}
+app.get('*', function(req, res) {res.send("404: not found")})
+
+server.on("request", (req, res) => {
+if (bare.shouldRoute(req)) {bare.routeRequest(req, res)} else {app(req, res)}})
+server.listen({port: PORT}, () => {})
--- static/config.js
+++ static/config.js
@@ -1,12 +1,11 @@
 const htmlConfig = {
-    gamesPath: "/g.html",
-    creditsPath: "/credits.html",
-    changeLogPath: "/ChangeLog.html",
+    gamesPath: "/ga",
+    creditsPath: "/credits",
+    changeLogPath: "/Changelog.html",
     homePagePath: "/index.html",
-    moviesPath: "/movies.html",
-    searchPath: "/p.html",
-    settingsPath: "/settings.html",
-    appsPath: "/a.html",
+    searchPath: "/ta",
+    settingsPath: "/settings",
+    appsPath: "/app",
 }
 
 const siteConfig = {
