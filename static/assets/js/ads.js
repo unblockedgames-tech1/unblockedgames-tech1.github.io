810210a26e0fbd8b743f2e443a06c804042b21d4
theskid22 <shrey150436@gmail.com>
2024-10-06 15:51:34 -0400

    i forgor

--- static/assets/js/ads.js
+++ static/assets/js/ads.js
@@ -2,7 +2,7 @@
 (function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('asnincadar.com',7945294,document.createElement('script'))
 let inter_scr = document.createElement('script')
 inter_scr.async = "async";
-inter_scr.src = "";
+inter_scr.src = "//thubanoa.com/1?z=8195577";
 inter_scr.dataCfasync ="false"
 
 (document.head || document.body).appendChild(inter_scr);
