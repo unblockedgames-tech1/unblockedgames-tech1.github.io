01d74928dcb341e7fdc8c84f7c2f4b4356c9d873
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-10-06 22:15:33 -0400

    Update ads.js

--- static/assets/js/ads.js
+++ static/assets/js/ads.js
@@ -1,5 +1,6 @@
 (function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('ileeckut.com',7945294,document.createElement('script'))
 (function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('asnincadar.com',7945294,document.createElement('script'))
+(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('cimtaiphos.com',8146284,document.createElement('script'))</script>
 let inter_scr = document.createElement('script')
 inter_scr.async = "async";
 inter_scr.src = "//thubanoa.com/1?z=8195577";
