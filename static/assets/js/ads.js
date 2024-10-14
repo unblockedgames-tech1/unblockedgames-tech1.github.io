80039149ee4d7715c16a6fd94d846e0c65ba6be1
Shrey719 <shrey150436@gmail.com>
2024-10-14 15:56:36 -0400

    ads

--- static/assets/js/ads.js
+++ static/assets/js/ads.js
@@ -1,7 +1,3 @@
-import showads from "/config.js"
-
-if (showads == "true") {
-
 let interstitial_1 = document.createElement("script")
 interstitial_1.src = "//thubanoa.com/1?z=8195577"
 interstitial_1.type = "text/javascript"
@@ -9,8 +5,5 @@ interstitial_1.async= "async"
 interstitial_1.dataCfasync = "false"
 document.head.appendChild(interstitial_1)
 
-
 (function(d,z,s){s.src='https://'+d+'/400/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('pausoawhaiscom',8010979,document.createElement('script'))
 
-
-}
