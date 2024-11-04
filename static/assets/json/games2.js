8633b22f3c2e61be9384fb8e3ee6c15d015d73a5
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-03 20:03:15 -0500

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -4,8 +4,6 @@ const games2Data = [
     imgSrc: "https://lh7-us.googleusercontent.com/3MEZuozpQz1-FCmISS10dsb71FRtbq7-MFLJzXAdOIrj55BeyX7gv5jhLCx9SsWOnKP0e_kEsXp7YQgB8jOXvP6CAEs-ixviwzbJl___b1iQ0dyTejQZPODm_1SoHF-Vgve9uK1dk9le0WKuWPrma2DUmA-8XA",
     link: __uv$config.prefix + __uv$config.encodeUrl("https://www.hoodamath.com/mobile/games/opposite-day/game.html?nocheckorient=1")
 },
-{
-
 
    ];
 export default gamesData;
