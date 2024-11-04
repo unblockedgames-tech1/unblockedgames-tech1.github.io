bfed54dcd03b78153e98a834cd7466cc1c385383
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-03 20:03:28 -0500

    Update gamesManager.js

--- static/assets/json/gamesManager.js
+++ static/assets/json/gamesManager.js
@@ -3,8 +3,3 @@ import games2Data from './games2.js';
 
 // Combine all game lists
 const allGames = [...gamesData, ...games2Data];
-
-// Optional: Sort games alphabetically by title
-const sortedGames = allGames.sort((a, b) => a.title.localeCompare(b.title));
-
-export default sortedGames;
