6c67acc531e71674b20fd4d43a99156dea926e67
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-03 19:51:29 -0500

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -5,20 +5,6 @@ const games2Data = [
     link: __uv$config.prefix + __uv$config.encodeUrl("https://www.hoodamath.com/mobile/games/opposite-day/game.html?nocheckorient=1")
 },
 {
-    title: "Slope Run", 
-    imgSrc: "https://lh7-us.googleusercontent.com/Gb8hdT5a8UzWV0lU4WZx7LWViY8SqULyzwwjQcfBA43vo-pMKOcveBSuk0_633Rb9pkR-UHxF9ItMsdLYdLs1qrDlw0vGK-1YJrEaqExpcIDhLz-XQdAs87JnT4bop6cvD-DJ9VPiUihOi8t_pBsXIXztj5bxw",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.hoodamath.com/mobile/games/slope-run/game.html?nocheckorient=1")
-},
-{
-    title: "Glitch", 
-    imgSrc: "https://lh7-us.googleusercontent.com/GzeRPIVD-t7QLRuxTzU9Z5FgA5i865PMFpk7zCjeCAvMcXeJ8u9ldbYhRvrFNDoIKutdpam9gerqE9FeL66KgU9AHTywG2EgTu618J263vorM2Zfhqo3oh8gDf9dLxh0nZICbvPwCH0mQO1asF9nn7JWnBsI9A",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.hoodamath.com/mobile/games/glitch/game.html?nocheckorient=1")
-},
-{
-    title: "Devil Trap Path", 
-    imgSrc: "https://lh7-us.googleusercontent.com/OIeUt1vNqld6fK6oVByterHRUNV9bz0OFKWmkJSdJUFPshHS6J-NhhnvtzZifb3xfd4SFLgcSSFoATPV-3Uc3F1V4xPHn8iUWOdxqD58vv0qTHFOlCOLW2h3xvJJSPdwTeByKhF-FHN9sb6AdOIeF24oPgcjTQ",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://storage.y8.com/y8-studio/html5/Gani/level-devil-trap-path/?key=y8&value=default")
-},
 
 
 ];
