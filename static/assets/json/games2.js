06a5170a17f6006f78a0ce1645f54724dfaaf6b2
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-03 19:37:24 -0500

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -4,15 +4,7 @@ const gamesData = [
         imgSrc: "gameimg/example.png",
         link: __uv$config.prefix + __uv$config.encodeUrl("https://example.com/game")
     }
-];
 
-export default games2Data;
-
-// gamesManager.js
-import gamesData from './games.js';
-import games2Data from './games2.js';
-
-// Combine all game lists
-const allGames = [...gamesData, ...games2Data];
-
-export default sortedGames;
+    
+   ];
+export default gamesData;
