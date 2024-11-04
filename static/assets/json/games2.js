288eca6619bfd838ebf0009f0448ac4c4a3f201d
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-03 20:23:26 -0500

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -1,8 +1,8 @@
 const games2Data = [
 {
-    title: "Opposite Day", 
-    imgSrc: "https://lh7-us.googleusercontent.com/3MEZuozpQz1-FCmISS10dsb71FRtbq7-MFLJzXAdOIrj55BeyX7gv5jhLCx9SsWOnKP0e_kEsXp7YQgB8jOXvP6CAEs-ixviwzbJl___b1iQ0dyTejQZPODm_1SoHF-Vgve9uK1dk9le0WKuWPrma2DUmA-8XA",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.hoodamath.com/mobile/games/opposite-day/game.html?nocheckorient=1")
+    title: "Mortal Combat", 
+    imgSrc: "https://lh7-us.googleusercontent.com/EprdGr8i7Kx5YsXuHwOx0Hov2efq_KzZtCf9pUMFBTrAQSR_96EbUc3m8snCMKg0q4p5Om_iJ6msLMBFGzfz0FBrCsdhtTeFmi1FV9nnw4EEb72M_91CegE6lvIs3o7oaX_igro6uVhUGfUIhKes8TmZBad5hw",
+    link: __uv$config.prefix + __uv$config.encodeUrl("https://static.arcadespot.com/retroemulator.php?system=sega&game=2017/03/mortal-kombat.gen")
 },
 
    ];
