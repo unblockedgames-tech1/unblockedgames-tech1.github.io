11d5ce7ba9834338b1d53894691f10919d971593
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-03 19:19:12 -0500

    Delete static/assets/json/game2.js

--- static/assets/json/game2.js
+++
@@ -1,9 +0,0 @@
-const additionalGames = [
-{
-    title: "Among us",
-    imgSrc: "gameimg/Amoungus.png",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://classroom.lol/_games/amongus/") 
-},
-];
-
-export default additionalGames;
