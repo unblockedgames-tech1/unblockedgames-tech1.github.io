5183952bb9a9b51e233b4241a79789cf5cf8c722
Shrey <164104873+Shrey719@users.noreply.github.com>
2024-11-03 21:36:37 -0500

    dear lord

--- static/assets/json/games.js
+++ static/assets/json/games.js
@@ -600,5 +600,15 @@ const gamesData = [
     imgSrc: "https://lh7-us.googleusercontent.com/OIeUt1vNqld6fK6oVByterHRUNV9bz0OFKWmkJSdJUFPshHS6J-NhhnvtzZifb3xfd4SFLgcSSFoATPV-3Uc3F1V4xPHn8iUWOdxqD58vv0qTHFOlCOLW2h3xvJJSPdwTeByKhF-FHN9sb6AdOIeF24oPgcjTQ",
     link: __uv$config.prefix + __uv$config.encodeUrl("https://storage.y8.com/y8-studio/html5/Gani/level-devil-trap-path/?key=y8&value=default")
 },
+{
+    title: "Mortal Combat", 
+    imgSrc: "https://lh7-us.googleusercontent.com/EprdGr8i7Kx5YsXuHwOx0Hov2efq_KzZtCf9pUMFBTrAQSR_96EbUc3m8snCMKg0q4p5Om_iJ6msLMBFGzfz0FBrCsdhtTeFmi1FV9nnw4EEb72M_91CegE6lvIs3o7oaX_igro6uVhUGfUIhKes8TmZBad5hw",
+    link: __uv$config.prefix + __uv$config.encodeUrl("https://oldgameshelf.com/emulator?id=L3JvbXMvbjY0L01vcnRhbCBLb21iYXQgNC9Nb3J0YWwgS29tYmF0IDQuemlw&console=n64&v=9.1")
+},
+{
+    title: "Pokemon Quetzal", 
+    imgSrc: "https://lh7-us.googleusercontent.com/y7FZT_XunMUl5bQaUk6W00FjVx5xaoGaPM2XtjNMI6xGO-8Qqasu0Ba2BASWlxadGLqAXkJlmTT5XSZ90EmtywFjsvlpOW5ksVMeP2aZlhgLT0feBQm0rNisa3VvsYAIMw94sVPWjGJD2-R6X3MsMqA-RvT3WQ",
+    link: __uv$config.prefix + __uv$config.encodeUrl("https://f.kbhgames.com/emujs/play.php?c=gba&f=PokemonQuetzalAlpha6v9.gba")
+}
    ];
 export { gamesData }; // Changed to named export
