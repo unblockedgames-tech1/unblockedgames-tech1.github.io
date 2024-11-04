eab0f05c7a5e85d54a2af03153e78d22be2f1a08
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-03 20:09:27 -0500

    Update gamesManager.js

--- static/assets/json/gamesManager.js
+++ static/assets/json/gamesManager.js
@@ -1,5 +1,10 @@
 import gamesData from './games.js';
 import games2Data from './games2.js';
 
-// Combine all game lists
+console.log("Games data 1:", gamesData);
+console.log("Games data 2:", games2Data);
+
 const allGames = [...gamesData, ...games2Data];
+console.log("Combined games:", allGames);
+
+export default allGames;
