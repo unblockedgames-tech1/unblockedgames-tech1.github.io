dfc18a22f4574fca71ad8fa4d5641e8f1deb2964
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-04 16:53:07 -0500

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -1,4 +1,8 @@
 const games2Data = [
-// ur braindead
+{
+    title: "Geomatry Dash", 
+    imgSrc: "https://lh7-us.googleusercontent.com/xzhOGM4wjIJKEp5B80TXpg4LArOEXtTVosOj98RMbNdzdtQBMitXKakY6kzTuaHJQoJa2TI--LQseSXSaGecSUUYRbACHQ3-M0R87sXCC_nWy0q0ovn8sXna5ZbHDSZKQBccX1w95MT5KwGdjoMm5wuTIKHiOw",
+    link: __uv$config.prefix + __uv$config.encodeUrl("https://geometrygame.org/game/geometry-dash-lite/")
+}
    ];
 export { games2Data }; // Named export
