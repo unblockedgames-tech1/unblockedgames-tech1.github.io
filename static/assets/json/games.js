0e58a683405a0d15ee9a246e0851f8a9b9d55fee
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-03 19:28:23 -0500

    Update games.js

--- static/assets/json/games.js
+++ static/assets/json/games.js
@@ -1,6 +1,4 @@
-import gamesFromFile2 from '/assets/json/games.js';
-
-const mainGames = [
+const gamesData = [
     {
         title: "Agar.io",
         imgSrc: "gameimg/Agario.png",
@@ -582,9 +580,26 @@ const mainGames = [
     imgSrc: "https://lh7-us.googleusercontent.com/ur4VB3U3kGEVoFAiha3Ay_h9rubAOXsSUJvXVxywP63P5TMrBrB6jauVFTwxlarQG9Vyv-ApDxsyYBPcA5f2BJAXddsz49fYeJZuoh3Q4EQDe2BcOqb2MkxB5o5TZiJi4TxFE2wvjFG2OU5mvYfUfwnKaCKPug",
     link: __uv$config.prefix + __uv$config.encodeUrl("https://now.gg/apps/a/10020/b.html")
 },
+{
+    title: "Opposite Day", 
+    imgSrc: "https://lh7-us.googleusercontent.com/3MEZuozpQz1-FCmISS10dsb71FRtbq7-MFLJzXAdOIrj55BeyX7gv5jhLCx9SsWOnKP0e_kEsXp7YQgB8jOXvP6CAEs-ixviwzbJl___b1iQ0dyTejQZPODm_1SoHF-Vgve9uK1dk9le0WKuWPrma2DUmA-8XA",
+    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.hoodamath.com/mobile/games/opposite-day/game.html?nocheckorient=1")
+},
+{
+    title: "Slope Run", 
+    imgSrc: "https://lh7-us.googleusercontent.com/Gb8hdT5a8UzWV0lU4WZx7LWViY8SqULyzwwjQcfBA43vo-pMKOcveBSuk0_633Rb9pkR-UHxF9ItMsdLYdLs1qrDlw0vGK-1YJrEaqExpcIDhLz-XQdAs87JnT4bop6cvD-DJ9VPiUihOi8t_pBsXIXztj5bxw",
+    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.hoodamath.com/mobile/games/slope-run/game.html?nocheckorient=1")
+},
+{
+    title: "Glitch", 
+    imgSrc: "https://lh7-us.googleusercontent.com/GzeRPIVD-t7QLRuxTzU9Z5FgA5i865PMFpk7zCjeCAvMcXeJ8u9ldbYhRvrFNDoIKutdpam9gerqE9FeL66KgU9AHTywG2EgTu618J263vorM2Zfhqo3oh8gDf9dLxh0nZICbvPwCH0mQO1asF9nn7JWnBsI9A",
+    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.hoodamath.com/mobile/games/glitch/game.html?nocheckorient=1")
+},
+{
+    title: "Devil Trap Path", 
+    imgSrc: "https://lh7-us.googleusercontent.com/OIeUt1vNqld6fK6oVByterHRUNV9bz0OFKWmkJSdJUFPshHS6J-NhhnvtzZifb3xfd4SFLgcSSFoATPV-3Uc3F1V4xPHn8iUWOdxqD58vv0qTHFOlCOLW2h3xvJJSPdwTeByKhF-FHN9sb6AdOIeF24oPgcjTQ",
+    link: __uv$config.prefix + __uv$config.encodeUrl("https://storage.y8.com/y8-studio/html5/Gani/level-devil-trap-path/?key=y8&value=default")
+},
 
    ];
-// Combine both game lists
-const gamesData = [...mainGames, ...gamesFromFile2];
-
 export default gamesData;
