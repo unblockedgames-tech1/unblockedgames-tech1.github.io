525f14d4096127b02c8e30b2b34b8591ab7bd456
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-03 19:33:29 -0500

    Create games2.js

---
+++ static/assets/json/games2.js
@@ -0,0 +1,20 @@
+// games2.js
+const games2Data = [
+    // Your second set of games will go here
+    {
+        title: "Example Game",
+        imgSrc: "gameimg/example.png",
+        link: __uv$config.prefix + __uv$config.encodeUrl("https://example.com/game")
+    }
+];
+
+export default games2Data;
+
+// gamesManager.js
+import gamesData from './games.js';
+import games2Data from './games2.js';
+
+// Combine all game lists
+const allGames = [...gamesData, ...games2Data];
+
+export default sortedGames;
