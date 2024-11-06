a43cca476bf71d979cd2840c339bde2343d61c72
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-06 08:32:44 -0500

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -8,6 +8,11 @@ const games2Data = [
     title: "Real Flight Sim", 
     imgSrc: "https://lh7-us.googleusercontent.com/UIiQ-6cWcHvKszc2Cjf66S4VhUgRjnA8nBcd717nC1E02Jx0cUhcnpBfk3CQilbg8WWGOdcl0dJsfYiDKEbXMbpLKZMH0rTlwSwG8rw-qwaLeUEeVXzy2cizsbIqQ0uxjXAwuTOOJ3VSbCUvNyBvtYcfSNO2Tg",
     link: __uv$config.prefix + __uv$config.encodeUrl("https://www.onlinegames.io/games/2023/unity/real-flight-simulator/index.html")
+},
+{
+    title: "BeamNG.Drive", 
+    imgSrc: "https://lh7-us.googleusercontent.com/9vqbywLNDkMjHo-Ner61tKa4ma-FHHgsDIJndIBrr-u4vAORC8eTSgxwy0JOrwGIdHZaOPukgezsi8uHlCyF5iUvaRSpZrdZ5ebi7dHRw2Potr_Pq-sy-GBAEB7kSxwjfs9l3gJOeo391ohW2tGicz-HJJrIIw",
+    link: __uv$config.prefix + __uv$config.encodeUrl("https://html5.gamedistribution.com/rvvASMiM/b3670406708c45b1be6b3c5d41e555de/index.html?gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL3d3dy5nYW1lZmxhcmUuY29tLyIsInBhcmVudERvbWFpbiI6ImdhbWVmbGFyZS5jb20iLCJ0b3BEb21haW4iOiJnYW1lZmxhcmUuY29tIiwiaGFzSW1wcmVzc2lvbiI6dHJ1ZSwibG9hZGVyRW5hYmxlZCI6dHJ1ZSwiaG9zdCI6Imh0bWw1LmdhbWVkaXN0cmlidXRpb24uY29tIiwidmVyc2lvbiI6IjEuNS4xNyJ9")
 }
    ];
 export { games2Data }; // Named export
