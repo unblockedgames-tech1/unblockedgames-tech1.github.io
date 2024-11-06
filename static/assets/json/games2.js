0d4ebd27db7a909583a98a722b696c4789162c0b
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-06 11:51:36 -0500

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -14,5 +14,10 @@ const games2Data = [
     imgSrc: "https://lh7-us.googleusercontent.com/9vqbywLNDkMjHo-Ner61tKa4ma-FHHgsDIJndIBrr-u4vAORC8eTSgxwy0JOrwGIdHZaOPukgezsi8uHlCyF5iUvaRSpZrdZ5ebi7dHRw2Potr_Pq-sy-GBAEB7kSxwjfs9l3gJOeo391ohW2tGicz-HJJrIIw",
     link: __uv$config.prefix + __uv$config.encodeUrl("https://www.gamearter.com/game/car-crash-test/")
 }
+{
+    title: "Brawl Stars", 
+    imgSrc: "https://lh7-us.googleusercontent.com/-dRY2l1fsCn90KR8MzgCxRpuQTsWvf-KBLXGLPAYH-rhVxIk70bGryGL-1X7qLAB7PClAuWyjs2z_e8SpRBnjH1iE7nORVC3eIdNOBKezaKOspU4whO01gqFEY5Bhcrt6ffZlmcyIIanL1s6pUSuyAqNXYdm0g",
+    link: __uv$config.prefix + __uv$config.encodeUrl("https://app-253517.games.s3.yandex.net/253517/9hp1grekl1t1ryv0jw8jyhm1ftagk69g/index.html?sdk=%2Fsdk%2F_%2Fv2.6397cd80c6de66c0367d.js#origin=https%3A%2F%2Fyandex.com&app-id=253517&device-type=desktop")
+},
    ];
 export { games2Data }; // Named export
