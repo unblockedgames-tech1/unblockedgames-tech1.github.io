9b7a2519d5a5bd4c519d09824be720c12b62db9d
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-06 08:20:57 -0500

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -3,11 +3,11 @@ const games2Data = [
     title: "Geomatry Dash", 
     imgSrc: "https://lh7-us.googleusercontent.com/xzhOGM4wjIJKEp5B80TXpg4LArOEXtTVosOj98RMbNdzdtQBMitXKakY6kzTuaHJQoJa2TI--LQseSXSaGecSUUYRbACHQ3-M0R87sXCC_nWy0q0ovn8sXna5ZbHDSZKQBccX1w95MT5KwGdjoMm5wuTIKHiOw",
     link: __uv$config.prefix + __uv$config.encodeUrl("https://geometrygame.org/game/geometry-dash-lite/")
-}
+},
 {
     title: "Real Flight Sim", 
     imgSrc: "https://lh7-us.googleusercontent.com/UIiQ-6cWcHvKszc2Cjf66S4VhUgRjnA8nBcd717nC1E02Jx0cUhcnpBfk3CQilbg8WWGOdcl0dJsfYiDKEbXMbpLKZMH0rTlwSwG8rw-qwaLeUEeVXzy2cizsbIqQ0uxjXAwuTOOJ3VSbCUvNyBvtYcfSNO2Tg",
     link: __uv$config.prefix + __uv$config.encodeUrl("https://www.onlinegames.io/games/2023/unity/real-flight-simulator/index.html")
-},
+}
    ];
 export { games2Data }; // Named export
