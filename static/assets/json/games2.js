1ab188a62446caa24adb1c8d84d72405d0919171
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-06 09:31:03 -0500

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -12,7 +12,7 @@ const games2Data = [
 {
     title: "BeamNG.Drive", 
     imgSrc: "https://lh7-us.googleusercontent.com/9vqbywLNDkMjHo-Ner61tKa4ma-FHHgsDIJndIBrr-u4vAORC8eTSgxwy0JOrwGIdHZaOPukgezsi8uHlCyF5iUvaRSpZrdZ5ebi7dHRw2Potr_Pq-sy-GBAEB7kSxwjfs9l3gJOeo391ohW2tGicz-HJJrIIw",
-    link: __uv$config.prefix + __uv$config.encodeUrl("https://html5.gamedistribution.com/rvvASMiM/b3670406708c45b1be6b3c5d41e555de/index.html?gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL3d3dy5nYW1lZmxhcmUuY29tLyIsInBhcmVudERvbWFpbiI6ImdhbWVmbGFyZS5jb20iLCJ0b3BEb21haW4iOiJnYW1lZmxhcmUuY29tIiwiaGFzSW1wcmVzc2lvbiI6dHJ1ZSwibG9hZGVyRW5hYmxlZCI6dHJ1ZSwiaG9zdCI6Imh0bWw1LmdhbWVkaXN0cmlidXRpb24uY29tIiwidmVyc2lvbiI6IjEuNS4xNyJ9")
+    link: __uv$config.prefix + __uv$config.encodeUrl("https://www.gamearter.com/game/car-crash-test/")
 }
    ];
 export { games2Data }; // Named export
