e621c456ad8f9610e1af507f526df2423470fc4b
Void Network <163482926+NeverBlockedGames@users.noreply.github.com>
2024-11-15 22:37:39 -0500

    Update games2.js

--- static/assets/json/games2.js
+++ static/assets/json/games2.js
@@ -69,6 +69,11 @@ const games2Data = [
     imgSrc: "https://lh7-us.googleusercontent.com/OF4fKFD4AbftL81PKtjYbFnTgxWDpth5EEhxICylwAwwJqKIyFKQjTL05HxlQyxbvr-jW8EXGMP-MK5nFM1QozZ_2Qyed8pwTGmPmUhw2UqX7YtKTx5iMldSY9Rg1kVCZ9y5OWUmwlOJVZ_x-G-9WC-fPJbsLA", 
     link: __uv$config.prefix + __uv$config.encodeUrl("https://www.coolmathgames.com/sites/default/files/public_games/47589/")
 },
+{
+    title: "Rodha",
+    imgSrc: "https://lh7-us.googleusercontent.com/OF4fKFD4AbftL81PKtjYbFnTgxWDpth5EEhxICylwAwwJqKIyFKQjTL05HxlQyxbvr-jW8EXGMP-MK5nFM1QozZ_2Qyed8pwTGmPmUhw2UqX7YtKTx5iMldSY9Rg1kVCZ9y5OWUmwlOJVZ_x-G-9WC-fPJbsLA", 
+    link: "/local games/tiny_fishing"
+},
 
 
    ];
